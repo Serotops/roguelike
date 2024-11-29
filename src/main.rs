92685084b3b71cca7ae65a248296ec566144365d
Spark <sparkismad@gmail.com>
2024-11-29 01:51:00 +0100

    Add inputs movement keys

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 // actual size of the window
 const SCREEN_WIDTH: i32 = 80;
@@ -8,6 +10,34 @@ const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
 struct Tcod {
     root: Root,
+}
+
+fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
+    // todo: handle keys
+    let key: Key = tcod.root.wait_for_keypress(true);
+    match key {
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+        Key {
+            code: Enter,
+            alt: true,
+            ..
+        } => {
+            // Alt+Enter: toggle fullscreen
+            let fullscreen: bool = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+        tcod::input::Key {
+            code: tcod::input::KeyCode::Escape,
+            ..
+        } => return true, // exit game
+        _ => {}
+    }
+
+    false
 }
 
 fn main() {
@@ -21,12 +51,21 @@ fn main() {
         .init();
 
     let mut tcod: Tcod = Tcod { root };
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
 
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
         tcod.root.flush();
         tcod.root.wait_for_keypress(true);
+
+        // handle keys and exit game if needed
+        let exit: bool = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
 }
